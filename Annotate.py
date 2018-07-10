96123644cd6c2417aa5d23b811170a53ec2bd6d4
Jacob Evans <jtevans.94@gmail.com>
2018-07-10 13:02:07 -0400

    added method to run prodigal

--- Annotate.py
+++ Annotate.py
@@ -6,18 +6,25 @@
 #	       representative
 #	4. hmm search eggNOG, pfam,tigrfam
 import click
+import subprocess
+from os.path import basename 
 
 @click.group()
 def main():
     pass
 
 @main.command()
-def annotate():
-    pass
-@main.command()
-def prodigal():
+def annotate_wf():
     pass
 
+@main.command()
+@click.argument('fasta', nargs=-1)
+def prodigal(fasta):
+    #prodigal -d out_nuc.fasta -i inFile.fasta -o outputFile.txt -s geneScores.txt
+    for inFasta in fasta:
+        inBaseName = basename(inFasta).split(".")[0]
+        print("calling genes with prodigal")
+        subprocess.call(['prodigal','-d',inBaseName + '_genes.fna','-i',inFasta,'-o',inBaseName+'_genes.gbk','-s',inBaseName + "geneInfo.txt", '-q'])
 @main.command()
 def proteinOrtho():
     pass
