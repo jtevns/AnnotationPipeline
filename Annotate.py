8eb73efead0df5037cbc6e3552d5b46fbbb9686e
Jacob Evans <jtevans.94@gmail.com>
2018-07-10 15:23:00 -0400

    working on proteinortho, figure out how to pass all fnas

--- Annotate.py
+++ Annotate.py
@@ -25,9 +25,13 @@ def prodigal(fasta):
         inBaseName = basename(inFasta).split(".")[0]
         print("calling genes with prodigal")
         subprocess.call(['prodigal','-d',inBaseName + '_genes.fna','-i',inFasta,'-o',inBaseName+'_genes.gbk','-s',inBaseName + "geneInfo.txt", '-q'])
+
 @main.command()
-def proteinOrtho():
-    pass
+@click.argument('fnas',nargs=-1)
+@click.argument('project',nargs=1)
+def protienOrtho(fnas,project):
+        #../software/proteinortho_v5.16b/proteinortho5.pl -project=Sample_99669_DAStool_bin15 -clean Sample_99669_DAStool_bin15_genes.fna combined_lm_DAStool_bin41_genes.fna
+        subprocess.call(['../software/proteinortho_v5.16b/proteinortho5.pl','-project',project,'-clean'])
 
 @main.command()
 def selectLongest():
