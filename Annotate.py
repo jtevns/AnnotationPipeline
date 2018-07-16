2a273ec714515b5d3b02b6524105137e29427160
Jacob Evans <jtevans.94@gmail.com>
2018-07-16 14:01:36 -0400

    added functionality for selecting the largest contig from cluster and writing to fasta

--- Annotate.py
+++ Annotate.py
@@ -8,7 +8,8 @@
 import click
 import subprocess
 from os.path import basename 
-
+from Bio import SeqIO 
+from Bio.SeqRecord import SeqRecord
 # commands callable by user
 @click.group()
 def main():
@@ -25,7 +26,7 @@ def prodigal(fasta):
     for inFasta in fasta:
         inBaseName = basename(inFasta).split(".")[0]
         print("calling genes with prodigal on " + inFasta)
-        subprocess.call(['prodigal','-a',inBaseName + '_genes.faa','-i',inFasta,'-o',inBaseName+'_genes.gbk','-s',inBaseName + "_geneInfo.txt", '-q'])
+        subprocess.call(['prodigal','-a',inBaseName + '_genes.faa','-i',inFasta,'-o', '-q'])
 
 @main.command()
 @click.argument('faas',nargs=-1)
@@ -40,9 +41,41 @@ def proteinOrtho(faas,project):
     makeOrthClusters(str(project) + ".proteinortho",project)
 
 @main.command()
-def selectRepresentative():
-    pass
-
+@click.argument('faas',nargs = -1)
+@click.argument('project',nargs = 1)
+def selectRepresentatives(faas,project):
+    #make a dictoinary of gene sizes
+    geneSizes = dict()
+    geneSeqs = dict()
+    for faa in faas:
+        for record in SeqIO.parse(faa,"fasta"):
+            geneSizes[faa+":"+record.id] = len(record.seq)
+            geneSeqs[faa+":"+record.id] = record.seq
+    #select the longest gene from each cluster
+    with open(str(project + "_proteinOrthoClusters.tsv")) as clusters:
+        with open(project + "_representatives.faa","w+") as outFile:
+           for cluster in clusters:
+               longestID = "unset"
+               longestLen = 0
+               #lengths = [geneSizes[gene] for gene in cluster.split()]
+               for gene in cluster.split("\t"):
+                   temp = gene.strip().split(":")
+                   binID = temp[0]
+                   if "," in temp[1]:
+                       contigs = temp[1].split(",")
+                       for contig in contigs:
+                           currContig = binID+":"+contig
+                           currLen = geneSizes[currContig]
+                           if currLen > longestLen:
+                               longestID = currContig
+                               longestLen = currLen
+                   else:
+                       currContig = gene.strip()
+                       currLen = geneSizes[currContig]
+                       if currLen > longestLen:
+                           longestID = currContig
+                           longestLen = currLen
+               SeqIO.write(SeqRecord(geneSeqs[longestID],id = longestID,description=""),outFile,'fasta')
 @main.command()
 def hmmer():
     pass
@@ -51,17 +84,17 @@ def hmmer():
 def makeOrthClusters(proteinClusters,project):
     with open(proteinClusters) as clusters:
         with open(str(project) + "_proteinOrthoClusters.tsv","w+") as outfile:
-            clusterCount = 1
             for line in clusters:
                 if(line.startswith("#")):
                     header = line.split()[4:-1]
                 else:
                     clusterLine = line.split("\t")[3:-1]
+                    combinedClusterRow = list()
                     for lineLoc in range(0,len(clusterLine)): 
-                        clusterRow = str(clusterCount) +"\t"+ header[lineLoc] +"\t"+ clusterLine[lineLoc]
+                        clusterRow = header[lineLoc] +":"+ clusterLine[lineLoc]
                         if "*" not in clusterRow:
-                            outfile.write(clusterRow + "\n")
-                clusterCount = clusterCount + 1
+                            combinedClusterRow.append(clusterRow)
+                    outfile.write("\t".join(combinedClusterRow) + "\n")
             
             
 
