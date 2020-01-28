6d9e9d868e1a825d0fc24e749383f4d8ceffaa36
jtevns <jtevans.94@gmail.com>
2020-01-28 17:28:05 +0000

    moved make_cluster_file to standalone py script

---
+++ make_cluster_file.py
@@ -0,0 +1,24 @@
+import sys
+
+#function to make cluster file
+def makeOrthClusters(proteinClusters):    
+    clusterCount = 0
+    clusters = open(proteinClusters, 'r')
+    outfile = open("ProteinOrthoClusters.tsv","w+")
+    for line in clusters:
+        if(line.startswith("#")):
+            header = line.split()[4:]
+        else:
+            clusterLine = line.split("\t")[3:]
+            clusterLine[-1] = clusterLine[-1].strip()
+            combinedClusterRow = list()
+            for lineLoc in range(0,len(clusterLine)):
+                clusterRow = header[lineLoc] +":"+ clusterLine[lineLoc]
+                if "*" not in clusterRow:
+                    combinedClusterRow.append(clusterRow)
+            outfile.write(str(clusterCount)+ "\t" + "\t".join(combinedClusterRow) + "\n")
+            clusterCount += 1
+    clusters.close()
+    outfile.close()
+
+makeOrthClusters(sys.argv[1])
